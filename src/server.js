55316517f01999413f66907a602050556a242587
jerrynim <tjerry3@naver.com>
2019-03-10 17:18:18 +0900

    add prisma

---
+++ src/server.js
@@ -0,0 +1,14 @@
+require("dotenv").config();
+import { GraphQLServer } from "graphql-yoga";
+import schema from "./schema";
+import logger from "morgan";
+
+const PORT = process.env.PORT || 4000;
+
+const server = new GraphQLServer({ schema });
+
+server.express.use(logger("dev"));
+
+server.start({ port: PORT }, () =>
+  console.log(`Server running on http://localhost:${PORT}`)
+);

2bf2ccdff70c861013ed487b128f9e44c1913403
jerrynim <tjerry3@naver.com>
2019-03-11 14:22:51 +0900

    sending SMS

--- src/api/User/requestSecret/requestSecret.js
+++ src/api/User/requestSecret/requestSecret.js
@@ -8,8 +8,7 @@ export default {
       try {
         await prisma.updateUser({ data: { loginSecret }, where: { email } });
         return true;
-      } catch (error) {
-        console.log(error);
+      } catch {
         return false;
       }
     }
--- src/server.js
+++ src/server.js
@@ -1,7 +1,13 @@
-require("dotenv").config();
+import dotenv from "dotenv";
+import path from "path";
+dotenv.config({ path: path.join(__dirname, ".env") });
+
 import { GraphQLServer } from "graphql-yoga";
 import schema from "./schema";
 import logger from "morgan";
+import { sendSecretMail } from "./utils";
+
+sendSecretMail("tjerry3@naver.com", "123");
 
 const PORT = process.env.PORT || 4000;
 

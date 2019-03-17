bd151a6bcfbeaabf46daa3dc92434f574bff8a51
jerrynim <tjerry3@naver.com>
2019-03-17 16:41:56 +0900

    upload Post

--- src/api/Post/seeFullPost/seeFullPost.js
+++ src/api/Post/seeFullPost/seeFullPost.js
@@ -1,5 +1,5 @@
 import { prisma } from "../../../../generated/prisma-client";
-import { COMMENT_FRAGMENT, FULL_POST_FRAGMENT } from "../../../fragments";
+import { FULL_POST_FRAGMENT } from "../../../fragments";
 
 export default {
   Query: {

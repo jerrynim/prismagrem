7377b7119ae91d0712b3cac7bf77f58b5d1eca74
jerrynim <tjerry3@naver.com>
2019-03-18 12:50:19 +0900

    seeRooms

--- src/fragments.js
+++ src/fragments.js
@@ -1,6 +1,7 @@
 export const USER_FRAGMENT = `
     id
     username
+    avatar
 `;
 
 export const COMMENT_FRAGMENT = `
@@ -29,6 +30,29 @@ export const FULL_POST_FRAGMENT = `
         }
         user {
             ${USER_FRAGMENT}
+        }
+    }
+`;
+
+export const MESSAGE_FRAGMENT = `
+    id
+    text
+    to{
+        ${USER_FRAGMENT}
+    }
+    from{
+        ${USER_FRAGMENT}
+    }
+`;
+
+export const ROOM_FRAGMENT = `
+    fragment PostParts on Room{
+        id
+        participants{
+        ${USER_FRAGMENT}
+        }
+        messages{
+            ${MESSAGE_FRAGMENT}
         }
     }
 `;

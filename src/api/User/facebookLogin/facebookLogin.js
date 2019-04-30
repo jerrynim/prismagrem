f5b2f569412b8e170ecb15d7932ee09badc85f65
jerrynim <tjerry3@naver.com>
2019-04-30 15:49:58 +0900

    secret not required && facebook login

---
+++ src/api/User/facebookLogin/facebookLogin.js
@@ -0,0 +1,28 @@
+import { prisma } from "../../../../generated/prisma-client";
+import { generateToken } from "../../../utils";
+
+export default {
+  Mutation: {
+    facebookLogin: async (_, args) => {
+      const { email, firstName, lastName, name } = args;
+      const existUser = await prisma.user({ email });
+      console.log(existUser);
+      if (existUser) {
+        //로그인 토큰을발행
+        const token = generateToken(existUser.id);
+        return token;
+      } else {
+        //user create
+        const newUser = await prisma.createUser({
+          email,
+          firstName,
+          lastName,
+          username: name
+        });
+        console.log(newUser);
+        const token = generateToken(newUser.id);
+        return token;
+      }
+    }
+  }
+};

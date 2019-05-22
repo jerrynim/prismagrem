f2f88b5fd1a181ee7c7be2905814ce8381049750
jerrynim <tjerry3@naver.com>
2019-05-22 14:48:35 +0900

    upload to aws S3

--- src/upload.js
+++ src/upload.js
@@ -1,7 +1,40 @@
-import { bodyParser } from "body-parser";
-
+import IncomingForm from "formidable";
+import aws from "aws-sdk";
+import uuid from "uuid/v1";
+import { createReadStream } from "fs";
 const upload = (req, res) => {
-  console.log(req.bodyParser());
-  return res.json();
+  let form = new IncomingForm();
+
+  form.on("file", async (field, file) => {
+    // Do something with the file
+    // e.g. save it to the database
+    // you can access it using file.path
+    const s3 = new aws.S3({
+      accessKeyId: process.env.ACCESSKEYID,
+      secretAccessKey: process.env.SECRET_ACCESSKEYID,
+      params: {
+        Bucket: "jerrynim-instagram"
+      }
+    });
+    const key = uuid() + "-" + file.name;
+    const stream = createReadStream(file.path);
+    console.log(stream);
+    // Upload to S3
+    const response = await s3
+      .upload({
+        Key: key,
+        ACL: "public-read",
+        Body: stream
+      })
+      .promise()
+      .catch();
+
+    const url = response.Location;
+    console.log(url);
+  });
+  form.on("end", () => {
+    res.json();
+  });
+  form.parse(req);
 };
 export default upload;
